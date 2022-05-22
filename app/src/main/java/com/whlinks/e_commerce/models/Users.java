b1d9ee1629ae7baeb2bcf16e3f9b09639fa62fd3
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-05-22 12:39:31 +0530

    Kamlesh

---
+++ app/src/main/java/com/whlinks/e_commerce/models/Users.java
@@ -0,0 +1,63 @@
+package com.whlinks.e_commerce.models;
+
+public class Users {
+    String email, password, fName, lName, phone, gender;
+
+    Users(String email.String password, String fName, String lName, String phone, String gender) {
+        this.email = email;
+        this.password = password;
+        this.fName = fName;
+        this.lName = lName;
+        this.phone = phone;
+        this.gender = gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+}
--- app/src/main/java/com/whlinks/e_commerce/service/CommonDBCall.java
+++ app/src/main/java/com/whlinks/e_commerce/service/CommonDBCall.java
@@ -1,6 +1,8 @@
 package com.whlinks.e_commerce.service;
 
 import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -10,27 +12,48 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.whlinks.e_commerce.ui.user.UserHomeActivity;
 
 public class CommonDBCall {
-    FirebaseAuth mAuth= FirebaseAuth.getInstance();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser firebaseUser = mAuth.getCurrentUser();
-  public void register(String email, String password, Context context){
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+    // Registration
+    public void register(String email, String password, Context context) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-
+                if (task.isComplete()) {
+                    firebaseFirestore.collection("User").document(firebaseUser.getUid()).set().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Intent intent = new Intent(context, UserHomeActivity.class);
+                            context.startActivity(intent);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(context, e + "", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
             }
 
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-
+                Toast.makeText(context, e + "", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
-    public void login(String email, String password, Context context){
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+    // Login
+
+    public void login(String email, String password, Context context) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
 
