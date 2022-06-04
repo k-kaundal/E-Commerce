52daa5dd00d3ddd0b514272ef780eb1b3996eb0e
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-06-05 00:06:34 +0530

    Kamlesh

--- app/src/main/java/com/whlinks/e_commerce/ui/MainActivity.java
+++ app/src/main/java/com/whlinks/e_commerce/ui/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
 
-
-
                 return false;
             }
         });
